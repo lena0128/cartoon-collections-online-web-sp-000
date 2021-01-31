5f8c4ad3719f0c506ec5667a6449bcc8ba4767a8
Lena Hou <lenahou2013@gmail.com>
2021-01-31 16:55:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,8 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_cheeses)  # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.cheese.includes?(cheese_types)
 end

633ff9cf1b9b4a6cdf4dabfb29e0dfbe4f17209f
Lena Hou <lenahou2013@gmail.com>
2021-01-31 17:03:46 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.each_with_index { |dwarf, index|
+    puts "#{index + 1} #{dwarf}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
   # Your code here
+  planeteer_calls.collect { |call|
+    call.capitalize << "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+  if call.length > 4
+     true
+  else
+     false
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.detect { |snack| cheese_types.include?(snack) }
 end

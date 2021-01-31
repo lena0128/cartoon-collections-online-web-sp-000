7aa345347749886968e1f9d28c3a250c76f186fc
Lena Hou <lenahou2013@gmail.com>
2021-01-31 16:59:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,5 +23,5 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.detect(cheese_types)
+  snacks.detect { |snack| snack.includes?(cheese_types) }
 end
